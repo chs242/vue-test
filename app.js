6f4077b62f45b1268d6786c5517422d7d4f10db1
Edward Stern <edwardstern242@gmail.com>
2019-12-03 15:30:22 +0000

    exercise completed

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 new Vue ({
     el: '#app',
     data: {
-        
+        toShow: true,
+        message: ""
     },
     methods: {
-
+        handleClick (){
+            this.toShow = false
+        }
     }
 })
 
@@ -12,9 +15,9 @@ new Vue ({
 
 
 
-
+// build a small app when
 //The user enters secret and clicks on Reveal
-//The input and the button disappear
-//In the same time, the protected content appears
+//The input and the button disappear-----------------
+//In the same time, the protected content appears---------------
 
-//Data, methods, v-if, v-else, v-model, v-on or @.
+//[Data}, methods, [v-if, v-else], v-model, @.
